788c3181fa628be1ef8fb494de60d8a687845602
JHansonKAttah92 <joshuahanson500@gmail.com>
2026-09-09 19:29:14 +0100

    Initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,79 @@
+import "./Navbar.css";
+import Hero from "../hero/Hero";
+import About from "../about/About";
+import Skills from "../skills/Skills";
+import Projects from "../projects/Projects";
+import Contact from "../contact/Contact";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { useState } from "react";
+import { MenuIcon } from "lucide-react";
+
+function Navbar() {
+  const [display, setDisplay] = useState();
+
+  const handleDisplay = () => {
+    if (window.innerWidth === 480) {
+      setDisplay();
+    }
+  };
+
+  return (
+    <BrowserRouter>
+      <nav className="navbar">
+        <div className="nav-container">
+          <h2 className="nav-logo">
+            <NavLink to="/" className="logo-link">
+              Hanson.Dev
+            </NavLink>
+          </h2>
+          <MenuIcon
+            className="menuIcon"
+            size={48}
+            color="#2563EB"
+            strokeWidth={1}
+          />
+          <ul className="nav-menu">
+            <li className="nav-item"></li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/skills" className="nav-link">
+                Skills
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/projects" className="nav-link">
+                Projects
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/contact" className="nav-link">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+          <a
+            href="http://"
+            target="_blank"
+            className="resume"
+            rel="noopener noreferrer"
+          >
+            Resume
+          </a>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Navbar;
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,11 @@
+
+
+function Skills() {
+    return (
+        <div className="skills">
+
+        </div>
+    )
+}
+
+export default Skills;
