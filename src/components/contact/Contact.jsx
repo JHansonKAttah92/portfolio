1c92cfe08f8a4ad2edbade67dd9f8480cc431ca0
JHansonKAttah92 <joshuahanson500@gmail.com>
2026-09-21 17:22:54 +0100

    Content was added to to contact component and small changes was made to the other components styles

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,20 @@
-
+import './Contact.css'
 
 function Contact() {
     return (
-        <div className="contact">
-
+        <div id="contact">
+            <span>Contact</span>
+            <div className="contact-content">
+                <h2>Let's work together</h2>
+                <p>Have progect in mind or just want to say hi? My inbox is open.</p>
+                <form className="action">
+                    <input type="text" className="name" aria-label="name"
+                    placeholder="Your name" required />
+                    <input type="email" className="email" aria-label="email" placeholder="name@company.com" required />
+                    <textarea name="textarea" className="textarea" aria-label="textarea" placeholder="Tell me about your project" required></textarea>
+                    <button className="btn">Send message</button>
+                </form>
+            </div>
         </div>
     )
 }
